1c48fab7ae3a8f7769915a0638b91b3e748d4a38
Rui Ribeiro <rui.ribeiro@fccn.pt>
2020-02-24 13:00:27 +0000

    Layout da Homepage

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,13 +4,13 @@
 function openNav() {
   document.getElementById("menu-container").style.cssText = "left:0px;";
   document.getElementById("open-menu-arrow").style.cssText = "display:none";
-  document.getElementById("close-menu-arrow").style.cssText = "display:block;z-index:2;";
+  document.getElementById("close-menu-arrow").style.cssText = "display:block !important;z-index:2;";
   document.getElementById("menu-overlay").style.cssText = "display:block;z-index:0;background: #000; opacity:0.5;";
 }
 
 function closeNav() {
   document.getElementById("menu-container").style.cssText = "left:-375px";
-  document.getElementById("menu-overlay").style.cssText = "display:block;z-index:-1;";
+  document.getElementById("menu-overlay").style.cssText = "display:none !important;z-index:-1;";
   document.getElementById("open-menu-arrow").style.cssText = "display:block;";
   document.getElementById("close-menu-arrow").style.cssText = "display:none;";
 }
