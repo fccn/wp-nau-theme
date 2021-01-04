d56ab2a3c7fade43d503ec01514834df359b7bc6
Sandro Costa <sandro.costa@fccn.pt>
2021-01-04 12:02:19 +0000

    Added new styles to action top menu Changed render method for search bar on topnav Fixed search results

--- partials/content-search.php
+++ partials/content-search.php
@@ -10,7 +10,9 @@
 ?>
 <div class="result">    
     <div class="link_image_wrapper"><a class="link_image" href="<?php echo get_permalink($post->ID)?>"><img class="image" src="<?php echo $image_url?>"></a></div>
+    <div class="result-details">
     <div class="title"><a href="<?php echo get_permalink($post->ID)?>"><?php echo $post->post_title?></a></div>
     <div class="excerpt"><?php echo get_the_excerpt($post)?><div class="tags"><?php echo nau_list_tags()?><?php echo nau_list_categories()?></div></a></div>
+    </div>
 </div>
 
