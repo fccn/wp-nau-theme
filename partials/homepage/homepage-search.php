0bccf07d39218f1bfa83580cadacaff23a9d4a48
Sandro Costa <sandro.costa@fccn.pt>
2021-05-17 17:52:31 +0100

    Add stylesheet to new homepage search bar Fix italic <em> style

---
+++ partials/homepage/homepage-search.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Homepage main search bar
+ * shortcode: [nau_homepage_search]
+ */
+?>
+
+<div id="homepage-form-container" class="homepage-searchbar">
+    <form class="homepage-search-form" action="<?php echo home_url(); ?>" role="search" method="post" enctype="application/x-www-form-urlencoded">
+        <label class="homepage-search-form-label" for="s"><?php echo nau_trans("Search Courses")?></label>
+        <input class="homepage-search-form-input" name="s" type="search" placeholder="<?=nau_trans("Search Courses")?>" value="<?php echo isset($search_query) ? $search_query : ""; ?>" aria-labelledBy="banner-search-form-label">
+        <input class="homepage-search-form-submit" name="submit-search" type="submit" value="<?php echo nau_trans("Search"); ?>" />
+    </form>
+</div>
