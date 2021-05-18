8db76d2ce11264c14fbc04db472ec1ba8c59ae64
Sandro Costa <sandro.costa@fccn.pt>
2021-05-18 10:45:20 +0100

    Search bar styles are now inline with other theme styles. Update translation for the search bar placeholder.

--- partials/homepage/homepage-search.php
+++ partials/homepage/homepage-search.php
@@ -8,7 +8,7 @@
 <div id="homepage-form-container" class="homepage-searchbar">
     <form class="homepage-search-form" action="<?php echo home_url(); ?>" role="search" method="post" enctype="application/x-www-form-urlencoded">
         <label class="homepage-search-form-label" for="s"><?php echo nau_trans("Search Courses")?></label>
-        <input class="homepage-search-form-input" name="s" type="text" placeholder="<?=nau_trans("Search Courses")?>" value="<?php echo isset($search_query) ? $search_query : ""; ?>" aria-labelledBy="banner-search-form-label">
+        <input class="homepage-search-form-input" name="s" type="text" placeholder="<?=nau_trans("What do you want to learn today?")?>" value="<?php echo isset($search_query) ? $search_query : ""; ?>" aria-labelledBy="banner-search-form-label">
         <input class="homepage-search-form-submit" name="submit-search" type="submit" value="<?php echo nau_trans("Search"); ?>" />
     </form>
 </div>
