8dd6d1c749212f1b207a2cfca30e13959a623e3b
Sandro Costa <sandro.costa@fccn.pt>
2021-05-24 12:20:50 +0100

    Fix for search method on Search forms

--- inc/global-top-menu.php
+++ inc/global-top-menu.php
@@ -41,7 +41,7 @@ $header_img_src = @$header_img_src_array[$lang] ?: $header_img_src_array['pt'];
       <div id="actions_container"> 
   
           <!-- starts top nav search bar -->          
-          <form id="search-form" action="<?php echo home_url(); ?>" role="search" method="post">
+          <form id="search-form" action="<?php echo home_url(); ?>" role="search" method="get">
             <label id="search-form-label" for="search-input"><?=nau_trans("Search Courses")?></label>
             <input id="search-input" name="s" type="search" placeholder="<?=nau_trans("Search courses...")?>"/>
             <input name="submit-search" type="submit" value="<?=nau_trans("Search Courses")?>">
--- partials/homepage/homepage-search.php
+++ partials/homepage/homepage-search.php
@@ -6,7 +6,7 @@
 ?>
 
 <div id="homepage-form-container" class="homepage-searchbar">
-    <form class="homepage-search-form" action="<?php echo home_url(); ?>" role="search" method="post" enctype="application/x-www-form-urlencoded">
+    <form class="homepage-search-form" action="<?php echo home_url(); ?>" role="search" method="get" enctype="application/x-www-form-urlencoded">
         <label class="homepage-search-form-label" for="s"><?php echo nau_trans("Search Courses")?></label>
         <input class="homepage-search-form-input" name="s" type="text" placeholder="<?=nau_trans("What do you want to learn today?")?>" value="<?php echo isset($search_query) ? $search_query : ""; ?>">
         <input class="homepage-search-form-submit" name="submit-search" type="submit" value="<?php echo nau_trans("Search"); ?>" />
