41d27dae23348ca372f3f8cc2fbd6071e54f7683
Sandro Costa <sandro.costa@fccn.pt>
2021-06-23 12:08:35 +0100

    Fix: Founder entity logos on homepage

--- partials/homepage/homepage-entities.php
+++ partials/homepage/homepage-entities.php
@@ -1,5 +1,5 @@
   <?php
-    $entities = nau_get_entities( ["filter" => "fundador"]);;
+    $entities = nau_get_entities( ["filter" => "fundador"]);
   ?>   
   
   <!-- starts entidades NAU -->
@@ -8,7 +8,7 @@
     <ul id="entities-nau-logos">
       <?php 
         foreach($entities as $entity):
-            
+
         $logo = get_custom_value("logo", "", $entity->ID);
       ?>        
       <li><a href="<?php echo get_permalink($entity)?>"><img src="<?php echo $logo?>" alt="<?php echo $entity->post_title?>" title="<?=nau_trans("Visit page of")?> <?php echo $entity->post_title?>"></a></li>      
