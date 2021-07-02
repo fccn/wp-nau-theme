afb2dfc403e2cb38b27af41c38ed697edfecb55d
Sandro Costa <sandro.costa@fccn.pt>
2021-07-02 11:21:00 +0100

    Feat (GN-350): Blog posts page pagination based on posts per page param in settings

--- blog.php
+++ blog.php
@@ -11,10 +11,11 @@ Template Name: Blog List Page
   
   include "inc/simple_header.php";
 
+  $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
   $query = new WP_Query(
     array( 
       'post_type' => 'post',
-      'posts_per_page' => -1,
+      'paged' => $paged
     )
   );
   
@@ -30,8 +31,27 @@ Template Name: Blog List Page
           get_template_part( 'partials/single', 'post');
 
         endwhile;
-      else:
     ?>
+      <div class="news-pagination">
+        <?php 
+            echo paginate_links( array(
+                'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+                'total'        => $query->max_num_pages,
+                'current'      => max( 1, get_query_var( 'paged' ) ),
+                'format'       => '?paged=%#%',
+                'show_all'     => false,
+                'type'         => 'plain',
+                'end_size'     => 2,
+                'mid_size'     => 1,
+                'prev_next'    => true,
+                'prev_text'    => sprintf( '<i></i> %1$s', nau_trans( 'Previous' ) ),
+                'next_text'    => sprintf( '%1$s <i></i>', nau_trans( 'Next' ) ),
+                'add_args'     => false,
+                'add_fragment' => '',
+            ) );
+        ?>
+      </div>
+  <? else: ?>
       <p><?php echo nau_trans('There are no available articles in this language.'); ?></p>
     <?php
       endif;
